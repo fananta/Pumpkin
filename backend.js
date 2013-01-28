3b3f8409d05ab55c0d90f17f78229a4e3ad0b1b8
Peter Le Bek <peter@hyperplex.org>
2013-01-28 17:00:09 -0500

    add function to create a topic

--- backend.js
+++ backend.js
@@ -6,6 +6,24 @@ var args = process.argv.splice(2);
 var port = parseInt(args[0]);
 if (isNaN(port)) port = 8000;
 
+/* Main datastore for topics, just a dictionary indexed by topicid */
+var topics = new Object();
+var next_topicid = 0;
+
+/* Create a new topic and return its id */
+function newTopic(text, link) {
+    topics[next_topicid] = { "text" : text, "link" : link };
+    return next_topicid++;
+}
+
+function runTests() {
+    console.log("/// Beginning of tests ///");
+    newTopic("Cool search engine", "http://google.com");
+    newTopic("Special domain", "http://example.com");
+    console.log(topics);
+    console.log("/// End of tests ///"); 
+}
+
 http.createServer(function(request, response) {
 	/* Serve index file */
         fs.readFile('./index.html', function(error, content) {
@@ -20,4 +38,5 @@ http.createServer(function(request, response) {
             });
     }).listen(port);
 
+runTests();
 console.log('Server running at http://127.0.0.1:' + port + '/');
